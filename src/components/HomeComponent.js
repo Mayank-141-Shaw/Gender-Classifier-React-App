70c6a23265400ac9e39deb957a31a8a6eb2034d0
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-10-28 20:14:20 +0530

    Pushing starter changes

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function HomeComponent() {
+  return (
+    <div style={{
+        fontSize:'50px', 
+        fontWeight:'bold',
+        color:'red',
+        padding:'auto',
+        textAlign:'center',
+        height:'100vh',
+        width:'100vw',
+        
+        backgroundColor:'yellow'
+    }}>
+        Home Page
+    </div>
+  )
+}
+
+export default HomeComponent
