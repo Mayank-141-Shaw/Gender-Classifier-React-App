8088f4c0a18c78a0dcd610e773d4aa6e7f986541
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-12-25 01:11:06 +0530

    Detail box

--- src/components/DetailsComponent.js
+++ src/components/DetailsComponent.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
 
 function DetailsComponent() {
+
+    const [isOpen, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen(!isOpen)
+    }
+
+    const DetailBox = styled.div`
+        width : auto;
+        height : fit-content;
+        border-radius : 10px;
+        background-color : pink;
+        margin : 0% 5% 5% 5%;
+    `;
+
   return (
-    <div>DetailsComponent</div>
+    <>
+        {
+            isOpen ? 
+            <DetailBox onClick={toggle}>Open</DetailBox>
+            :
+            <DetailBox onClick={toggle}>Closed</DetailBox>
+        }
+    </>
   )
 }
 
