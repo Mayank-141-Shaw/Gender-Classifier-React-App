05c2cc845decbc67ebaf6017a74a5eb02331ceab
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-12-25 00:22:45 +0530

    Update changes

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,20 +1,55 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import { MdAddCircle } from 'react-icons/md'
+import DetailsComponent from './DetailsComponent';
 
 function HomeComponent() {
+
+  const AddUserButton = styled.div`
+    position : fixed;
+    bottom : 10px;
+    right : 10px;
+  `;
+  
+  const Background = styled.div`
+    font-size : 50px;
+    font-weight : bold;
+    color : red;
+    padding : auto;
+    text-align : center;
+    height : 100vh;
+    width : 100vw;
+    background-color : grey;
+  `;
+
   return (
-    <div style={{
-        fontSize:'50px', 
-        fontWeight:'bold',
-        color:'blue',
-        padding:'auto',
-        textAlign:'center',
-        height:'100vh',
-        width:'100vw',
-        
-        backgroundColor:'yellow'
-    }}>
-        Home Page
-    </div>
+    <>
+
+      <Background>
+
+        <DetailsComponent />
+
+        <AddUserButton>
+          <NavLink to={"/add"} 
+            style={{
+              textDecoration:'none', 
+              color:'blue'
+            }} >
+
+            <MdAddCircle 
+              style={{
+                verticalAlign:'middle'
+              }} 
+              size={'2em'}
+            />
+
+          </NavLink>
+        </AddUserButton>
+
+      </Background>
+
+    </>
   )
 }
 
